6114e493598f93e6e422eb90e3d0473025a2f015
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-04-25 20:17:13 +0800

    loginbtn finished

---
+++ stage2/assignment0425/loginbtn.js
@@ -0,0 +1,14 @@
+const mainBodyBox = document.getElementById("main-body");
+// const signOutBtn = document.getElementById("login");
+
+const loginFunc = function () {
+  mainBodyBox.style.display == "block"
+    ? (mainBodyBox.style.display = "none")
+    : (mainBodyBox.style.display = "block");
+};
+
+const signOut = function () {
+  mainBodyBox.style.display == "block"
+    ? (mainBodyBox.style.display = "none")
+    : (mainBodyBox.style.display = "block");
+};
