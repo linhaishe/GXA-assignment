35726b0c30fdfe8f77cfc6c4cacb5ed086404448
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-05-12 10:15:46 +0800

    点击事件未完成

--- stage2/lcx-0511/danrudanchu.js
+++ stage2/lcx-0511/danrudanchu.js
@@ -10,7 +10,6 @@ function showOff() {
 
 function showOn() {
   imgs[n].className = "active";
-  // oLi[n].className = "active";
   for (var i = 0; i < oLi.length; i++) {
     oLi[i].className = "";
   }
@@ -31,6 +30,23 @@ var timer = setInterval(showFn, 2000);
 oDiv.onmouseover = function () {
   clearInterval(timer);
 };
+
 oDiv.onmouseout = function () {
   slideon = setInterval(showFn, 2000);
 };
+
+// for (var j = 0; j < oLi.length; j++) {
+//   oLi[j].dataset.index = j;
+//   oLi[j].onclick = function () {
+//     n = this.dataset.index;
+//     showFn();
+//   };
+// }
+
+// for (var j = 0; j < oLi.length; j++) {
+//   oLi[j].onclick = function () {
+//     slideOff(); //图片淡出
+//     n = this.innerHTML - 1;
+//     slideOn(); //图片淡出
+//   };
+// }
