ba1310998cb6543b564385870d04a833391cb7d3
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-05-14 11:47:05 +0800

    敌机出现，敌机运动，子弹射击，子弹移动

--- stage2/0513/planegame.js
+++ stage2/0513/planegame.js
@@ -2,20 +2,58 @@ var oStrat = document.querySelector("#strat");
 var oExit = document.querySelector("#exit");
 var oPlayBox = document.querySelector("#mainBody");
 var welcomePage = document.querySelector("#welcomepage");
+var myPlane;
+var toLeft = false;
+var toTop = false;
+var toRight = false;
+var toBottom = false;
+var bulletArr = [];
 
-oStrat.onclick = function () {
+function getRandomPlace(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+// oStrat.onclick = function () {
+//   welcomePage.style.display = "none";
+//   //   welcomepage.style.display = "none";
+//   oPlayBox.style.backgroundImage = "url('images/background_1.png')";
+
+//   myPlane = new NewPlane(
+//     "images/myplane.gif",
+//     mainBody.offsetWidth / 2 - 33,
+//     mainBody.offsetHeight - 80,
+//     8,
+//     1
+//   );
+//   setInterval(playerMove, 30);
+// };
+
+function startGame() {
   welcomePage.style.display = "none";
   //   welcomepage.style.display = "none";
   oPlayBox.style.backgroundImage = "url('images/background_1.png')";
 
-  new NewPlane(
+  myPlane = new NewPlane(
     "images/myplane.gif",
     mainBody.offsetWidth / 2 - 33,
     mainBody.offsetHeight - 80,
-    1,
+    8,
     1
   );
-};
+  setInterval(playerMove, 30);
+  setInterval(bulletMove, 30);
+  setInterval(function () {
+    new EnemyPlane(
+      "images/enemy1_fly_1.png",
+      getRandomPlace(0, oPlayBox.offsetWidth - 34),
+      -24,
+      4,
+      4
+    );
+  }, 500);
+}
+
+oStrat.addEventListener("click", startGame, false);
 
 oExit.onclick = function () {
   close();
@@ -38,4 +76,168 @@ NewPlane.prototype.init = function () {
   this.node.style.left = this.x + "px";
   this.node.style.top = this.y + "px";
   mainBody.appendChild(this.node);
+};
+
+//控制飞机上下左右飞行
+
+NewPlane.prototype.toTop = function () {
+  var t = this.node.offsetTop - this.speed;
+  if (t <= 0) {
+    t = 0;
+  }
+
+  this.node.style.top = t + "px";
+};
+
+NewPlane.prototype.toRight = function () {
+  var r = this.node.offsetLeft + this.speed;
+  if (r >= 254) {
+    r = 254;
+  }
+
+  this.node.style.left = r + "px";
+};
+
+NewPlane.prototype.toBottom = function () {
+  var btm = this.node.offsetTop + this.speed;
+  if (btm >= 488) {
+    btm = 488;
+  }
+
+  this.node.style.top = btm + "px";
+};
+
+NewPlane.prototype.toLeft = function () {
+  var l = this.node.offsetLeft - this.speed;
+  if (l <= 0) {
+    l = 0;
+  }
+
+  this.node.style.left = l + "px";
+};
+
+//子弹射击
+
+NewPlane.prototype.shoot = function () {
+  new Bullet(
+    "images/bullet1.png",
+    //不是myPlane.offsetLe，是myPlane.node.offsetLeft,node是img object
+    myPlane.node.offsetLeft + 30,
+    myPlane.node.offsetTop - 14,
+    11
+  );
+};
+
+//子弹的构造函数
+function Bullet(src, x, y, speed) {
+  this.src = src;
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.node = document.createElement("img");
+
+  this.init();
+}
+
+Bullet.prototype.init = function () {
+  this.node.src = this.src;
+  this.node.style.position = "absolute";
+  this.node.style.left = this.x + "px";
+  this.node.style.top = this.y + "px";
+  oPlayBox.appendChild(this.node);
+  bulletArr.push(this);
+};
+
+//子弹移动
+Bullet.prototype.move = function () {
+  //子弹消失
+  //此方法只是让界面的图片消失，没有消除节点和数组内的节点，节点会不断增加，需要实现子弹节点消失和节点移除两部分内容
+  var t = this.node.offsetTop - this.speed;
+  if (t <= -14) {
+    this.node.style.display = "none";
+  }
+  this.node.style.top = t + "px";
+  // this.node.style.top = this.node.offsetTop - this.speed + "px";
+};
+
+function bulletMove() {
+  for (var i = 0; i < bulletArr.length; i++) {
+    bulletArr[i].move();
+  }
+  // console.log("bulletArr", bulletArr);
+}
+
+function playerMove() {
+  if (toLeft) {
+    myPlane.toLeft();
+  }
+  if (toTop) {
+    myPlane.toTop();
+  }
+  if (toRight) {
+    myPlane.toRight();
+  }
+  if (toBottom) {
+    myPlane.toBottom();
+  }
+}
+
+//敌机构造函数
+function EnemyPlane(src, x, y, speed, blood) {
+  this.src = src;
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.blood = blood;
+  this.node = document.createElement("img");
+
+  this.init();
+}
+
+EnemyPlane.prototype.init = function () {
+  this.node.src = this.src;
+  this.node.style.position = "absolute";
+  this.node.style.left = this.x + "px";
+  this.node.style.top = this.y + "px";
+  oPlayBox.appendChild(this.node);
+};
+
+document.onkeydown = function () {
+  switch (event.keyCode) {
+    case 37:
+      toLeft = true;
+      break;
+    case 38:
+      toTop = true;
+      break;
+    case 39:
+      toRight = true;
+      break;
+    case 40:
+      toBottom = true;
+      break;
+    case 13:
+      startGame();
+      break;
+    case 87:
+      myPlane.shoot();
+      break;
+  }
+};
+
+document.onkeyup = function () {
+  switch (event.keyCode) {
+    case 37:
+      toLeft = false;
+      break;
+    case 38:
+      toTop = false;
+      break;
+    case 39:
+      toRight = false;
+      break;
+    case 40:
+      toBottom = false;
+      break;
+  }
 };
