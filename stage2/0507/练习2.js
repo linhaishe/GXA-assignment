27e733b4f65c1140b67a72cfcabcc0e3bb5745ee
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-05-21 15:19:20 +0800

    单个清除功能失败，原因因为获取不到a元素

--- stage2/0507/练习2.js
+++ stage2/0507/练习2.js
@@ -1,14 +1,53 @@
 var addBtn = document.querySelectorAll(".add");
-var clearAllBtn = document.querySelectorAll("#allclear");
+var clearAllBtn = document.querySelector("#allclear");
 var oLi = document.querySelectorAll("li");
+var textAreaDiv = document.querySelector("#textArea");
+
+//console.log(addBtn[1]);
 
 for (var i = 0; i < oLi.length; i++) {
   addBtn[i].onclick = function () {
+    var btn = event.target;
+    //获得文本内容
+    // console.log(btn.parentNode.childNodes[0].nodeValue);
+    var text = btn.parentNode.childNodes[0].nodeValue;
+    //创建节点
     var newDiv = document.createElement("div");
     var newALink = document.createElement("a");
-    //var text = this[i].parentNode.innerHTML;
-    var text = oLi[i];
-    console.log(text);
-    var addText = document.createTextNode("This is new.");
+
+    //创建内容
+    var textNode = document.createTextNode(text);
+    var deleteNode = document.createTextNode(" x");
+
+    //div添加class属性
+    newDiv.className = "textArea-list";
+
+    //div添加内容节点
+    newDiv.appendChild(textNode);
+
+    //a节点添加内容
+    newALink.appendChild(deleteNode);
+
+    //a 节点添加属性
+    newALink.href = "javascript:;";
+
+    //div内添加a节点
+    newDiv.appendChild(newALink);
+
+    //区域内添加文本
+    textAreaDiv.appendChild(newDiv);
   };
+
+  //   var aLink = document.querySelectorAll("a");
+
+  //   for (var j = 0; j < aLink.length; j++) {
+  //     aLink[j].onclick = function () {
+  //       // var parentNode = this.parentNode;
+  //       console.log(11111);
+  //     };
+  //   }
 }
+
+clearAllBtn.onclick = function () {
+  textAreaDiv.innerHTML = "";
+};
