c37830661e40d90ca783669c28f9ccc6be293a97
linhaishe <42030043+linhaishe@users.noreply.github.com>
2021-05-21 10:52:15 +0800

    debug

--- stage2/0507/练习2.js
+++ stage2/0507/练习2.js
@@ -6,7 +6,8 @@ for (var i = 0; i < oLi.length; i++) {
   addBtn[i].onclick = function () {
     var newDiv = document.createElement("div");
     var newALink = document.createElement("a");
-    var text = this[i].parentNode.innerHTML;
+    //var text = this[i].parentNode.innerHTML;
+    var text = oLi[i];
     console.log(text);
     var addText = document.createTextNode("This is new.");
   };
